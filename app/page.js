54f69d3d6cc81d0aa5f87661c09f04ec11df813e
Pablo Ríos <pablo_rios_p@protonmail.com>
2026-01-07 20:15:43 -0300

    Agregando link de ig y logo raster temporalmente

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex gap-3 min-h-screen items-center justify-center font-sans">
@@ -6,8 +8,19 @@ export default function Home() {
       </h1>
       <main>
         <p>El sitio está en construcción.</p>
-        <p>Sitio creado con NextJS</p>
+        <p>
+          Visita nuestro{" "}
+          <a
+            href="https://www.instagram.com/talkers.cl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+        </p>
+        <Image src="/talkers.cl.jpg" alt="Logo" width={200} height={200} />
       </main>
+      <footer>Sitio creado con NextJS</footer>
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
