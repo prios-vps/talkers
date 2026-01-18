6f8dd971aa1ebd03d0bb2dbf3a4301d2ca562dc9
Pablo Ríos <pablo_rios_p@protonmail.com>
2026-01-18 06:09:25 -0300

    Correcciones en distribución de espacio en main

--- app/page.jsx
+++ app/page.jsx
@@ -3,22 +3,24 @@ import ContactLinks from "@/components/contactLinks";
 export default function Home() {
   return (
     <>
-      <main className="flex-1 bg-pink-100">
-        <p className="uppercase text-7xl font-semibold p-7 text-white bg-green-400 text-center">Clases de Inglés</p>
-        <p className="uppercase text-6xl font-semibold p-7 text-white bg-blue-300 text-center">personalizadas</p>
-        <div className="flex-1 flex-row bg-white">
-          <img
-            className="flex bg-pink-500 h-80 w-80 p-10"
-            src="images/logo/Full logo ajustado.svg"
-            alt=""
-            width={200}
-            height={200}
-            aria-hidden="true"
-            focusable="false"
-          />
+      <main className="flex-1 flex-col bg-pink-100">
+        <p className="uppercase text-7xl font-semibold p-6 text-white bg-green-400 text-center">Clases de Inglés</p>
+        <p className="uppercase text-6xl font-semibold p-6 text-white bg-blue-300 text-center">personalizadas</p>
+        <div className="flex flex-row bg-green-500 justify-around align-center">
+          <div className="flex w-fit bg-pink-500 p-10">
+            <img
+              className="h-80 w-80"
+              src="images/logo/Full logo ajustado.svg"
+              alt=""
+              width={200}
+              height={200}
+              aria-hidden="true"
+              focusable="false"
+            />
+          </div>
           <ContactLinks />
         </div>
-        <p className="uppercase text-6xl font-semibold p-7 text-white bg-blue-300 text-center">para estudiantes y adultos</p>
+        <p className="uppercase text-6xl font-semibold p-6 text-white bg-blue-300 text-center">para estudiantes y adultos</p>
       </main>
     </>
   );
--- components/contactLinks.jsx
+++ components/contactLinks.jsx
@@ -4,7 +4,7 @@ import MailIcon from "./icons/mail";
 
 export default function ContactLinks() {
   return (
-    <section className="flex flex-col w-auto">
+    <section className="flex-1 flex-col w-fit bg-yellow-500">
       <a
         href="mailto:talkers.cl@gmail.com"
         target="_blank"
